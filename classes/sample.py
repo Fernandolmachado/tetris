f8727f051ca750c84c104bba81381e14f377a946
Fernandolmachado <flm.machado10@gmail.com>
2020-05-17 02:10:29 -0300

    Início do Projeto

---
+++ classes/sample.py
@@ -0,0 +1,97 @@
+# Author............: Fernando Machado
+# Date...............: 12/05/2020
+
+import pygame
+import sqlite3
+
+
+class Game:
+    """
+    Standard class with pygame structure
+    """
+    def __init__(self):
+        # Used to prevent high latency of buffer
+        # Arguments(frequency, size, channel, buffer)
+        # Channel argument: 1 mono, 2 stereo
+        # Buffer: must be a power of 2
+        pygame.mixer.pre_init(22100, -16, 2, 64)
+        # Inicialize pygame modules
+        pygame.init()
+
+        # Screen parameters
+        width = 1080
+        height = 1848
+        self.display = pygame.display.set_mode((width, height))
+
+        # App parameters
+        self.clock = pygame.time.Clock()
+        self.clock_tick = 20
+        self.run = False
+
+        # Sound instatiations
+        # sound = pygame.mixer.Sound("file")
+
+        self.restart()
+
+    def restart(self):
+        """
+        Class attributes that can be reseted
+        """
+
+        # Click control attributes
+        self.click_point = None
+        self.clicked = False
+
+    def game(self):
+        """
+        Main loop
+        """
+        self.setup()
+        while self.run:
+            self.clock.tick(self.clock_tick)
+            self.events()
+            self.render()
+            self.draws()
+
+    def setup(self):
+        """
+        Setup before loop
+        """
+        self.run = True
+
+    def events(self):
+        """
+        Events threats
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.click_point = event.pos
+                self.clicked = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                # assign False to self.clicked here, to implement
+                # continuous pushies
+                self.clicked = False
+
+    def render(self):
+        """
+        Manipulation of images and geometry
+        """
+        if self.clicked:
+            # assign False to self.clicked here, to prevent
+            # continuous pushies insteady of self.events
+            # self.clicked = False
+            pass
+        else:
+            pass
+
+    def draws(self):
+        """
+        Draw objects on screen
+        """
+        # Clear screen
+        self.display.fill()
+
+        # Update the screen with new shape
+        pygame.display.update()
